a26db0005e814a4a2a52f16804dc0743ff25245b
Deepak-sangeetham <71721706+Deepak-sangeetham@users.noreply.github.com>
2021-03-10 01:29:43 +0530

    Add files via upload

---
+++ musicplayer.h
@@ -0,0 +1,18 @@
+#ifndef _Music_player
+#define _Music_player
+
+void Play();
+void Pause();
+void Resume();
+
+void Delete();
+void DeleteAll();
+void PlayAll();
+
+void Next();
+void Previous();
+
+
+ 
+
+#endif
